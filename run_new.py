e9127c4e89926c0c1050438999b2ec837c6e41c0
Xinye-89 <85206164+Xinye-89@users.noreply.github.com>
2024-11-25 21:30:57 +0800

    Update run_new.py

--- run_new.py
+++ run_new.py
@@ -9,27 +9,27 @@ from models.detector import detector_eval
 
 import warnings
 warnings.filterwarnings('ignore')
-pd.set_option('display.max_rows', None)  # 设置为None表示无限制  
-pd.set_option('display.max_columns', None)  # 设置为None表示无限制  
+pd.set_option('display.max_rows', None)    
+pd.set_option('display.max_columns', None)  
 pd.set_option('display.width', None) 
 
-#----数据读取-----
+#----read data-----
 df_train=pd.read_csv('./dataset/MICD_6.csv',index_col=False)
 
-#-----------模型训练------------- 
-# 构建异构图
+#-----------train model------------- 
+# heterogeneous network construction
 le = LabelEncoder()
 df_train['patient_id2']=le.fit_transform(df_train['patient_id'])
 df_train['hospital_id2']=le.fit_transform(df_train['hospital_id'])+1+max(df_train['patient_id2'])
 G=generate_hetero_g(df_train) 
-# hash编码
+# hash coding
 hash1,hash2=hashFunc(df_train)
 # embedding
 feat_lst=['feat'+str(x) for x in range(43)]
 mne = MNE()
 feats_con = mne.fit(G, df_train[feat_lst].values, df_train, hash1, hash2)
 # detector
-thadm_result,thadm_eval = dbscan_detector(feats_con, result['labels'], eps=0.009, min_samples=2)
+thadm_result,thadm_eval = detector_eval(feats_con, df_train['label'], eps=0.009, min_samples=2)
 
 
 
@@ -74,21 +74,6 @@ thadm_result,thadm_eval = dbscan_detector(feats_con, result['labels'], eps=0.009
     
     
     
-    
-    
-    
-    
-    
-    
-    
-
-
-
-
-
-
-
-
 
 
 
